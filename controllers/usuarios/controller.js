faf7831836da994d74716e29f0cb955fbf8a5352
Jessica Moncada <jessmoncada1@gmail.com>
2021-10-17 00:45:25 -0500

    editar en proceso

--- controllers/usuarios/controller.js
+++ controllers/usuarios/controller.js
@@ -1,44 +1,50 @@
-import { getDB } from '../../db/conn.js'
+import { getDB } from "../../db/conn.js";
+import { ObjectId } from "mongodb";
 
-const crearUsuario = async (datosUsuarios, callback) => {
-  console.log(datosUsuarios)
-  console.log('llaves: ', Object.keys(datosUsuarios))
+const crearUsuario = async (datosUsuario, callback) => {
+  console.log(datosUsuarios);
+  console.log("llaves: ", Object.keys(datosProductos));
   if (
-    Object.keys(datosUsuarios).includes('nombre') &&
-    Object.keys(datosUsuarios).includes('apellido') &&
-    Object.keys(datosUsuarios).includes('cedula') &&
-    Object.keys(datosUsuarios).includes('email') &&
-    Object.keys(datosUsuarios).includes('rol') &&
-    Object.keys(datosUsuarios).includes('estado')
+    Object.keys(datosUsuarios).includes("nombre") &&
+    Object.keys(datosUsuarios).includes("apellido") &&
+    Object.keys(datoUsuarios).includes("cedula") &&
+    Object.keys(datosUsuarios).includes("email") &&
+    Object.keys(datosUsuarios).includes("estado") &&
+    Object.keys(datosUsuarios).includes("rol")
   ) {
-    const baseDeDatos = getDB()
-    // implementar código para crear usuario en la BD
-    await baseDeDatos.collection('usuarios').insertOne(datosUsuarios, callback)
+    const baseDeDatos = getDB();
+    // implementar código para crear vehículo en la BD
+    await baseDeDatos
+      .collection("usuarios")
+      .insertOne(datosUsuarios, callback);
   } else {
-    return 'error'
+    return "error";
   }
 };
 
 const getAllUsers = async (callback) => {
-  const baseDeDatos = getDB()
-  await baseDeDatos.collection('usuarios').find().limit(50).toArray(callback)
+  const baseDeDatos = getDB();
+  await baseDeDatos
+    .collection("usuarios")
+    .find({})
+    .limit(50)
+    .toArray(callback);
 };
 
 const editarUsuario = async (id, edicion, callback) => {
-  const filtroUsuario = { _id: new ObjectId(id) }
+  const filtroUsuario = { _id: new ObjectId(id) };
   const operacion = {
     $set: edicion,
-  }
-
-  const baseDeDatos = getDB()
+  };
+  const baseDeDatos = getDB();
   await baseDeDatos
-    .collection('usuarios')
+    .collection("usuarios")
     .findOneAndUpdate(
       filtroUsuario,
       operacion,
       { upsert: true, returnOriginal: true },
-      callback,
-    )
+      callback
+    );
 };
 
-export { crearUsuario, getAllUsers, editarUsuario }
+export { crearUsuario, getAllUsers, editarUsuario };
