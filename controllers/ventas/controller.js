a46e3345931acf02b17b3baa9d79089a22666136
Jessica Moncada <jessmoncada1@gmail.com>
2021-10-20 01:55:58 -0500

    listar y editar ventas

--- controllers/ventas/controller.js
+++ controllers/ventas/controller.js
@@ -1,21 +1,50 @@
 import { getDB } from "../../db/conn.js";
-
-
+import { ObjectId } from "mongodb";
 
 const crearVenta = async (datosVentas, callback) => {
-  console.log(datosVentas);
+  console.log(datosUsuarios);
   console.log("llaves: ", Object.keys(datosVentas));
   if (
-    Object.keys(datosVentas).includes("vendedor") &&
-    Object.keys(datosVentas).includes("producto") &&
-    Object.keys(datosVentas).includes("cantidad")
+    Object.keys(datosVentas).includes("valorTotal") &&
+    Object.keys(datosVentas).includes("fecha") &&
+    Object.keys(datosVentas).includes("documentoCliente") &&
+    Object.keys(datosVentas).includes("nombreCliente") &&
+    Object.keys(datosVentas).includes("nombreVendedor") &&
+    Object.keys(datosVentas).includes("productos")
   ) {
     const baseDeDatos = getDB();
-    // implementar código para crear vehículo en la BD
-    await baseDeDatos.collection('ventas').insertOne(datosVentas, callback);
+    // implementar código para crear ventas en la BD
+    await baseDeDatos
+      .collection("ventas2")
+      .insertOne(datosVentas, callback);
   } else {
     return "error";
   }
 };
 
-export default crearVenta;
+const getAllVentas = async (callback) => {
+  const baseDeDatos = getDB();
+  await baseDeDatos
+    .collection("ventas2")
+    .find({})
+    .limit(50)
+    .toArray(callback);
+};
+
+const editarVentas = async (id, edicion, callback) => {
+  const filtroVentas = { _id: new ObjectId(id) };
+  const operacion = {
+    $set: edicion,
+  };
+  const baseDeDatos = getDB();
+  await baseDeDatos
+    .collection("ventas2")
+    .findOneAndUpdate(
+      filtroVentas,
+      operacion,
+      { upsert: true, returnOriginal: true },
+      callback
+    );
+};
+
+export { crearVenta, getAllVentas, editarVentas };
--- server.js
+++ server.js
@@ -12,17 +12,6 @@ import jwks from 'jwks-rsa';
 const app = Express();
 dotenv.config({ path: "./.env" });
 
-// var jwtCheck = jwt({
-//   secret: jwks.expressJwtSecret({
-//       cache: true,
-//       rateLimit: true,
-//       jwksRequestsPerMinute: 5,
-//       jwksUri: 'https://dev-tic-team.us.auth0.com/.well-known/jwks.json'
-// }),
-// audience: 'api-gestor-ventas',
-// issuer: 'https://dev-tic-team.us.auth0.com/',
-// algorithms: ['RS256']
-// });
 
 app.use(Express.json());
 app.use(Cors());
