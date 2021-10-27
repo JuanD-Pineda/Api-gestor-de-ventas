0a958e551ded97101752debaf8e00475d71b001b
Jessica Moncada <jessmoncada1@gmail.com>
2021-10-27 17:23:08 -0500

    organizando puerto

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import jwks from "jwks-rsa";
 
 dotenv.config({ path: "./.env" });
 
-const puerto = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 const app = Express();
 app.use(Express.json());
 app.use(Cors());
@@ -36,7 +36,7 @@ app.use(rutasVentas);
 
 const main = () => {
   return app.listen(process.env.port, () => {
-    console.log(`escuchando puerto ${puerto}`);
+    console.log(`escuchando puerto ${port}`);
   });
 };
 
